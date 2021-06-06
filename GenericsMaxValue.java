67a85f2030d7e5477bcb582114d5a6e26e30ceb0
gitbash <kolhenagesh2017@gmail.com>
2021-06-06 11:52:40 +0530

    Added : Find maximum Integer Value

--- GenericsMaxValue.java
+++ GenericsMaxValue.java
@@ -1,7 +1,27 @@
-public class GenericsMaxValue {
+package com.generics;
 
+public class GenericsMaxValue {
 	public static void main(String[] args) {
-		System.out.println("Welcome");
+
+		Integer[] a = {4, 8, 5}; 
+		toPrint(a);
+		System.out.println("print maximum Value");
+		System.out.println(max(a));
+
+	}
+	public static <E extends Comparable<E>> E max(E... elements) {
+		E max = elements[0];
+		for (E element : elements) {
+			if (element.compareTo(max) > 0) {
+				max = element;
+			}
+		}
+		return max;
+	}
+	private static <E> void toPrint(E[] a) {
+
+		for (E i:a) {
+			System.out.println(i);  
+		}
 	}
 }
-
